bc03e21297bc9d33b3a3407cbc8df2ed2e79c140
Alexangel Sanchez <alexangel2323@gmail.com>
2022-05-19 19:43:08 -0500

    commit tree 7883b19f4d94d170852246bbd44a1221099da541

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+// Multiply 2 numers that equal to 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random()*(1000 - 0 + 1)+0);
+// Calculate a remainder that is equal to 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+// Return 20 as the highest number in the set
+const max = Math.max(2,4,6,8,10,12,14,16,18,20);
